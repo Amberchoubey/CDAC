16dad5023ca01f9932ea3cac369bc901f3326d1b
Amberchoubey <amberchoubey7890@gmail.com>
2024-10-01 23:19:20 +0530

    Update que1.java

--- JAVA/MamAssignment1/Assignment1/que1.java
+++ JAVA/MamAssignment1/Assignment1/que1.java
@@ -1,1 +1,11 @@
+public class WideningConversionDemo {
+    public static void main(String[] args) {
+      
+        int intValue = 42;
+
+        double doubleValue = intValue; 
+        System.out.println("Integer value: " + intValue);
+        System.out.println("Widened double value: " + doubleValue);
+    }
+}
 
